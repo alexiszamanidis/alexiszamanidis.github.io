d420091d45731ffa3c9d9324515c252a381e9220
Alexis Zamanidis <alexiszamanidis@outlook.com>
2020-12-12 15:10:39 +0200

    add trimString, calculateAge

---
+++ src/utilities/index.tsx
@@ -0,0 +1,24 @@
+export const trimString = (string: string, limit: number): string => {
+    return string != null
+        ? string.length <= limit
+            ? string
+            : string.substring(0, limit) + "..."
+        : "";
+};
+
+export const calculateAge = (dateOfBirth: string): number => {
+    let todayDate = new Date();
+    let todayDay = todayDate.getDate();
+    let todayMonth = todayDate.getMonth();
+    let todayYear = todayDate.getFullYear();
+
+    let birthDate = new Date(dateOfBirth);
+    let birthDay = birthDate.getDate();
+    let birthMonth = birthDate.getMonth();
+    let birthYear = birthDate.getFullYear();
+
+    let age = todayYear - birthYear;
+
+    if (todayMonth > birthMonth || (todayMonth === birthMonth && todayDay >= birthDay)) return age;
+    else return age - 1;
+};
