c3dccc4eb7688071e318750af5253e165622d16f
Alexis Zamanidis <alexiszamanidis@outlook.com>
2020-12-13 22:00:41 +0200

    fetch user data from github

--- src/components/AboutMe/LeftPaper/index.tsx
+++ src/components/AboutMe/LeftPaper/index.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
-import { aboutMe } from "./config";
 import { calculateAge } from "../../../utilities";
+import GitHub from "../../../services/GitHub";
+import Alert from "@material-ui/lab/Alert";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -27,23 +28,64 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+interface UserDataFields {
+    name: string;
+    avatar_url: string;
+    location: string;
+    bio: string;
+}
+
 function LeftPaper() {
     const classes = useStyles();
+
+    const [userData, setUserData] = useState<UserDataFields>({
+        name: "",
+        avatar_url: "",
+        location: "",
+        bio: "",
+    });
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        GitHub.getUserData("alexiszamanidis")
+            .then(({ data }) => {
+                setUserData({
+                    name: data.name,
+                    avatar_url: data.avatar_url,
+                    location: data.location,
+                    bio: data.bio,
+                });
+            })
+            .catch((error) => {
+                setError(error);
+            });
+    }, []);
 
     return (
         <Grid item xs={12} sm={6}>
             <Paper className={classes.paper}>
-                <Avatar alt="Alexis Zamanidis" src={aboutMe.avatar} className={classes.avatar} />
-                <div className={classes.item + " personal"}>
-                    <i className={classes.title + " fa fa-user"}></i>
-                    Alexis Zamanidis <i className={classes.title + " fa fa-map-marker"}></i>
-                    Athens, Greece <i className={classes.title + " fa fa-birthday-cake"}></i>
-                    {calculateAge("1998-01-20")}
-                </div>
-                <div className={classes.item}>
-                    <b className={classes.title}>Summary</b> <br />
-                    {aboutMe.summary}
-                </div>
+                {error === true ? (
+                    <Alert severity="error">Something happened</Alert>
+                ) : (
+                    <div>
+                        <Avatar
+                            alt="Alexis Zamanidis"
+                            src={userData.avatar_url}
+                            className={classes.avatar}
+                        />
+                        <div className={classes.item + " personal"}>
+                            <i className={classes.title + " fa fa-user"}></i>
+                            {userData.name} <i className={classes.title + " fa fa-map-marker"}></i>
+                            {userData.location}{" "}
+                            <i className={classes.title + " fa fa-birthday-cake"}></i>
+                            {calculateAge("1998-01-20")}
+                        </div>
+                        <div className={classes.item}>
+                            <b className={classes.title}>Summary</b> <br />
+                            {userData.bio}
+                        </div>
+                    </div>
+                )}
             </Paper>
         </Grid>
     );
