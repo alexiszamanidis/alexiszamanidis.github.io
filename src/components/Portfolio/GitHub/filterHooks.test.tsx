aeb13671c1b8e854fa2dcd161c4cdde0728e5685
Alexis Zamanidis <alexiszamanidis@outlook.com>
2021-04-08 22:53:03 +0300

    add useUniqueLanguages tests

--- src/components/Portfolio/GitHub/filterHooks.test.tsx
+++ src/components/Portfolio/GitHub/filterHooks.test.tsx
@@ -1,16 +1,16 @@
-import { useSearch } from "./filterHooks";
 import { act } from "react-dom/test-utils";
 import { renderHook } from "@testing-library/react-hooks";
+import { useSearch, useUniqueLanguages } from "./filterHooks";
 
 describe("useSearch", () => {
-    it("return content", () => {
+    it("should return content", () => {
         const { result } = renderHook(() => useSearch());
         expect(result.current.search).toBe("");
         expect(result.current.setSearch).toBeInstanceOf(Function);
         expect(result.current.handleSearch).toBeInstanceOf(Function);
     });
 
-    it("update search with useSearch", () => {
+    it("should update search with setSearch", () => {
         const { result } = renderHook(() => useSearch());
         act(() => {
             result.current.setSearch("test");
@@ -18,7 +18,7 @@ describe("useSearch", () => {
         expect(result.current.search).toBe("test");
     });
 
-    it("update search with handleSearch", () => {
+    it("should update search with handleSearch", () => {
         const { result } = renderHook(() => useSearch());
         act(() => {
             let event = {
@@ -27,5 +27,72 @@ describe("useSearch", () => {
             result.current.handleSearch(event);
         });
         expect(result.current.search).toBe("test");
+    });
+});
+
+type Repo = {
+    id: number;
+    name: string;
+    description: string;
+    language: string;
+    stargazers_count: number;
+    forks_count: number;
+    html_url: string;
+};
+
+describe("useUniqueLanguages", () => {
+    let data: Repo[] = [
+        {
+            id: 1,
+            name: "C Name",
+            description: "C Description",
+            language: "C Language",
+            stargazers_count: 0,
+            forks_count: 0,
+            html_url: "C url",
+        },
+        {
+            id: 2,
+            name: "C++ Name",
+            description: "C++ Description",
+            language: "C++ Language",
+            stargazers_count: 0,
+            forks_count: 0,
+            html_url: "C++ url",
+        },
+    ];
+
+    it("should return content", () => {
+        const { result } = renderHook(() => useUniqueLanguages(data));
+        expect(result.current.selectedLanguage).toBe("");
+        expect(result.current.setSelectedLanguage).toBeInstanceOf(Function);
+        expect(result.current.handleLanguage).toBeInstanceOf(Function);
+        expect(result.current.languages).toBeInstanceOf(Array);
+    });
+
+    it("should update selectedLanguage with setSelectedLanguage", () => {
+        const { result } = renderHook(() => useUniqueLanguages(data));
+        act(() => {
+            result.current.setSelectedLanguage("test");
+        });
+        expect(result.current.selectedLanguage).toBe("test");
+    });
+
+    it("should update search with handleSearch", () => {
+        const { result } = renderHook(() => useUniqueLanguages(data));
+        act(() => {
+            let event = {
+                target: { value: "test" },
+            } as React.ChangeEvent<{ value: string }>;
+            result.current.handleLanguage(event);
+        });
+        expect(result.current.selectedLanguage).toBe("test");
+    });
+
+    it("should return unique languages", () => {
+        const { result } = renderHook(() => useUniqueLanguages(data));
+        expect(result.current.languages.length).toBe(2);
+        expect(result.current.languages[0]).toBe("C Language");
+        expect(result.current.languages[1]).toBe("C++ Language");
     });
 });
