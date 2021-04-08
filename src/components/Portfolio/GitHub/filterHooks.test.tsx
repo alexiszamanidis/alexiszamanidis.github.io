794f68b0bb3654f7e4dcfe3e5c49ab0dc3481c0c
Alexis Zamanidis <alexiszamanidis@outlook.com>
2021-04-09 01:17:58 +0300

    add useFilteredData tests

--- src/components/Portfolio/GitHub/filterHooks.test.tsx
+++ src/components/Portfolio/GitHub/filterHooks.test.tsx
@@ -1,6 +1,6 @@
 import { act } from "react-dom/test-utils";
 import { renderHook } from "@testing-library/react-hooks";
-import { useSearch, useUniqueLanguages } from "./filterHooks";
+import { useFilteredData, useSearch, useUniqueLanguages } from "./filterHooks";
 
 describe("useSearch", () => {
     it("should return content", () => {
@@ -40,34 +40,44 @@ type Repo = {
     html_url: string;
 };
 
-describe("useUniqueLanguages", () => {
-    let data: Repo[] = [
-        {
-            id: 1,
-            name: "C Name",
-            description: "C Description",
-            language: "C Language",
-            stargazers_count: 0,
-            forks_count: 0,
-            html_url: "C url",
-        },
-        {
-            id: 2,
-            name: "C++ Name",
-            description: "C++ Description",
-            language: "C++ Language",
-            stargazers_count: 0,
-            forks_count: 0,
-            html_url: "C++ url",
-        },
-    ];
+let data: Repo[] = [
+    {
+        id: 1,
+        name: "C Name",
+        description: "C Description",
+        language: "C",
+        stargazers_count: 0,
+        forks_count: 0,
+        html_url: "C url",
+    },
+    {
+        id: 2,
+        name: "C++ Name",
+        description: "C++ Description",
+        language: "C++",
+        stargazers_count: 0,
+        forks_count: 0,
+        html_url: "C++ url",
+    },
+    {
+        id: 3,
+        name: "C++ Name 2",
+        description: "C++ Description 2",
+        language: "C++",
+        stargazers_count: 0,
+        forks_count: 0,
+        html_url: "C++ url 2",
+    },
+];
 
+describe("useUniqueLanguages", () => {
     it("should return content", () => {
         const { result } = renderHook(() => useUniqueLanguages(data));
         expect(result.current.selectedLanguage).toBe("");
         expect(result.current.setSelectedLanguage).toBeInstanceOf(Function);
         expect(result.current.handleLanguage).toBeInstanceOf(Function);
         expect(result.current.languages).toBeInstanceOf(Array);
+        expect(result.current.languages.length).toBe(2);
     });
 
     it("should update selectedLanguage with setSelectedLanguage", () => {
@@ -92,7 +102,57 @@ describe("useUniqueLanguages", () => {
     it("should return unique languages", () => {
         const { result } = renderHook(() => useUniqueLanguages(data));
         expect(result.current.languages.length).toBe(2);
-        expect(result.current.languages[0]).toBe("C Language");
-        expect(result.current.languages[1]).toBe("C++ Language");
+        expect(result.current.languages[0]).toBe("C");
+        expect(result.current.languages[1]).toBe("C++");
+    });
+});
+
+describe("useFilteredData", () => {
+    it("should return content", () => {
+        const { result } = renderHook(() => useFilteredData(data, "", ""));
+        expect(result.current.computedData).toBeInstanceOf(Array);
+        expect(result.current.computedData.length).toBe(3);
+    });
+
+    it("should filter data by debouncedSearch", () => {
+        const { result } = renderHook(() => useFilteredData(data, "C Name", ""));
+        expect(result.current.computedData.length).toBe(1);
+        expect(result.current.computedData[0]).toMatchObject({
+            id: 1,
+            name: "C Name",
+            description: "C Description",
+            language: "C",
+            stargazers_count: 0,
+            forks_count: 0,
+            html_url: "C url",
+        });
+    });
+
+    it("should filter data by selectedLanguage", () => {
+        const { result } = renderHook(() => useFilteredData(data, "", "C"));
+        expect(result.current.computedData.length).toBe(1);
+        expect(result.current.computedData[0]).toMatchObject({
+            id: 1,
+            name: "C Name",
+            description: "C Description",
+            language: "C",
+            stargazers_count: 0,
+            forks_count: 0,
+            html_url: "C url",
+        });
+    });
+
+    it("should filter data by debouncedSearch and selectedLanguage", () => {
+        const { result } = renderHook(() => useFilteredData(data, "C++ Name 2", "C++"));
+        expect(result.current.computedData.length).toBe(1);
+        expect(result.current.computedData[0]).toMatchObject({
+            id: 3,
+            name: "C++ Name 2",
+            description: "C++ Description 2",
+            language: "C++",
+            stargazers_count: 0,
+            forks_count: 0,
+            html_url: "C++ url 2",
+        });
     });
 });
