8397c5a4ac92efb46401b05dc28b06f60ef16266
Alexis Zamanidis <alexiszamanidis@outlook.com>
2021-04-09 21:50:48 +0300

    add useTabs test

--- src/components/useTabs.test.tsx
+++ src/components/useTabs.test.tsx
@@ -1,13 +1,17 @@
 import { useTabs } from "./useTabs";
 import { act } from "react-dom/test-utils";
 import { renderHook } from "@testing-library/react-hooks";
+import AboutMe from "./AboutMe";
+import Portfolio from "./Portfolio";
 
 describe("useTabs", () => {
     it("should return content", () => {
         const { result } = renderHook(() => useTabs());
         expect(result.current.value).toBe(0);
         expect(result.current.setValue).toBeInstanceOf(Function);
-        // expect(result.current.showTabContent).toBeInstanceOf(null);
+        let aboutMeComponent = <AboutMe />;
+        if (result.current.showTabContent !== null)
+            expect(result.current.showTabContent.type).toBe(aboutMeComponent.type);
     });
 
     it("should update value with setValue", () => {
@@ -16,5 +20,25 @@ describe("useTabs", () => {
             result.current.setValue(1);
         });
         expect(result.current.value).toBe(1);
+    });
+
+    it("should update value with setValue to 2 and showTabContent value to Portfolio component", () => {
+        const { result } = renderHook(() => useTabs());
+        act(() => {
+            result.current.setValue(1);
+        });
+        expect(result.current.value).toBe(1);
+        let portfolioComponent = <Portfolio />;
+        if (result.current.showTabContent !== null)
+            expect(result.current.showTabContent.type).toBe(portfolioComponent.type);
+    });
+
+    it("should update value with setValue to 2 and showTabContent value to null", () => {
+        const { result } = renderHook(() => useTabs());
+        act(() => {
+            result.current.setValue(2);
+        });
+        expect(result.current.value).toBe(2);
+        expect(result.current.showTabContent).toBe(null);
     });
 });
